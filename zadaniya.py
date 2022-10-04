8233a88ab7896a78173784996eb043b46479aabc
AdamKh <adamkhashiev2004@mail.ru>
2022-10-04 08:48:04 +0300

    fix

--- zadaniya.py
+++ zadaniya.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 if __name__ == '__main__':
     # 1
-    vowels = set('AaEeIiOoYy')
-    a = input('Введите строку\n')
-    s = set(a.replace(' ', ''))
+    str_in = input("Введите строку: ").lower()
+    a = set(str_in.replace(' ', ''))
+    vowels = set("aeiouyаоуыэеёиюяи")
+    count = 0
+    for i in str_in:
+        if i in vowels:
+            count += 1
+    print('Количество глассных в предложении = ', count)
 
     # 2
     a = input('Введите строку\n')
     s1 = set(a.replace(' ', ''))
     a = input('Введите строку\n')
     s2 = set(a.replace(' ', ''))
-    print(s1.intersection(s2))
+    print('Количество общих символов = ', s1.intersection(s2))
